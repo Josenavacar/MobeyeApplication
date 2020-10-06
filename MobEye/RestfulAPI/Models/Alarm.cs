6d6a3545522b51a9096cb0b5a7b78ec6f487876a
unknown <kostadinov.manolov@student.fontys.nl>
2020-10-06 06:23:03 -0700

    Modelo of Alarm added

--- MobEye/RestfulAPI/Models/Alarm.cs
+++ MobEye/RestfulAPI/Models/Alarm.cs
@@ -7,5 +7,27 @@ namespace RestfulAPI.Models
 {
     public class Alarm
     {
+        private string deviceName { get; set; }
+        private string location { get; set;}
+        private string alarmText { get; set; }
+        private int[] recipients { get; set; }
+        private string accountId { get; set; }
+
+        public Alarm(string deviceName, string location, string alarmText, int[] recipients, string accountId)
+        {
+            this.deviceName = deviceName;
+            this.location = location;
+            this.alarmText = alarmText;
+            this.recipients = recipients;
+            this.accountId = accountId;
+        }
+        public Alarm()
+        {
+            this.recipients = new int[20];
+        }
+
+       
+
+   
     }
 }
