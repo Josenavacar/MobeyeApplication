f334e92ea1033f98bcc37848079eda75bed2f1a6
Anastasia <434559@student.fontys.nl>
2020-10-08 15:24:10 +0200

    Added alarm page and basic controls
    To access, change the boolean's variable in the main App code

---
+++ MobEye/MobEye/MobEye/AlarmPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MobEye
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AlarmPage : ContentPage
+    {
+        public AlarmPage()
+        {
+            InitializeComponent();
+        }
+        void ConfirmAlarm(object sender, EventArgs e)
+        {
+            (sender as Button).Text = "Alarm confirmed";
+        }
+        void SkipAlarm(object sender, EventArgs e)
+        {
+            (sender as Button).Text = "Alarm skipped";
+        }
+    }
+}
