e7e5d792e907afbed1e1416ca5314b15e6e03408
Kareem <22612495+louiskareem@users.noreply.github.com>
2020-11-01 23:23:52 +0100

    Working on the bug: Connect mobile emulator to the api running on localhost (local device)

--- MobEye/MobEye/MobEye/Views/AlarmPage.xaml.cs
+++ MobEye/MobEye/MobEye/Views/AlarmPage.xaml.cs
@@ -9,14 +9,16 @@ using System.Net.Http;
 using Newtonsoft.Json;
 using System.Collections.ObjectModel;
 using MobEye.Models;
+using MobEye.Controls;
 
 namespace MobEye
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class AlarmPage : ContentPage
     {
-        private const String url = "https://localhost:44349/api/Alarm";
-        private HttpClient httpClient = new HttpClient();
+        private const String url = "https://10.0.2.2:44349/api/Alarm";
+        private HttpClient httpClient;
+        private HttpClientHandler clientHandler;
         private ObservableCollection<AlarmMessage> alarmMessages;
 
         public AlarmPage()
@@ -40,14 +42,66 @@ namespace MobEye
 
         protected override async void OnAppearing()
         {
-            //var content = await httpClient.GetStringAsync(url);
-            //var alarmMessage = JsonConvert.DeserializeObject<List<AlarmMessage>>(content);
+            //deviceName.Text = "hhelo";
+            //location.Text = "hhelo";
+            //alarmText.Text = "hhelo";
+            //accountId.Text = "hhelo";
 
-            //alarmMessages = new ObservableCollection<AlarmMessage>(alarmMessage);
+            clientHandler = new HttpClientHandler();
+            clientHandler.ServerCertificateCustomValidationCallback = (sender, cert, chain, sslPolicyErrors) => { return true; };
+            httpClient = new HttpClient(clientHandler);
 
-            //Message_List.ItemsSource = alarmMessages;
+            var content = await httpClient.GetStringAsync(url);
+            var newalarmMessage = JsonConvert.DeserializeObject<List<AlarmMessage>>(content);
+            alarmMessages = new ObservableCollection<AlarmMessage>(newalarmMessage);
+            Message_List.ItemsSource = alarmMessages;
+            base.OnAppearing();
 
-            //base.OnAppearing();
+            //var result = await GetApiResponse(url);
+
+            //if (result.IsSuccessful)
+            //{
+            //    try
+            //    {
+            //        var alarmMessage = JsonConvert.DeserializeObject<AlarmMessage>(result.JsonResponse);
+            //        deviceName.Text = alarmMessage.deviceName;
+            //        location.Text = alarmMessage.location;
+            //        alarmText.Text = alarmMessage.alarmText;
+            //        accountId.Text = alarmMessage.accountId;
+            //    }
+            //    catch (Exception ex)
+            //    {
+            //        await DisplayAlert("Error", ex.Message, "OK");
+            //    }
+            //}
+        }
+
+        public async Task<ApiResponse> GetApiResponse(String url)
+        {
+            clientHandler = new HttpClientHandler();
+            clientHandler.ServerCertificateCustomValidationCallback = (sender, cert, chain, sslPolicyErrors) => { return true; };
+
+            httpClient = new HttpClient(clientHandler);
+            using (httpClient)
+            {
+                // If response is good, then get the content, else return an erro message and reason of the error
+                var request = await httpClient.GetAsync(url);
+
+                if (request.IsSuccessStatusCode)
+                {
+                    return new ApiResponse
+                    {
+                        JsonResponse = await request.Content.ReadAsStringAsync()
+                    };
+                }
+                else
+                {
+                    return new ApiResponse
+                    {
+                        ErrorMessage = request.ReasonPhrase
+                    };
+                }
+            }
         }
     }
 }
