f217f8f2d2965e38e55bfd84bb063f33c3531512
Manolov <kostadinov.manolov@student.fontys.nl>
2020-11-04 13:04:10 +0100

    Added Unit test for Alarm class

--- MobEye/MobEye/MobEye/Models/User.cs
+++ MobEye/MobEye/MobEye/Models/User.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace MobEye.Models
 {
-    class User
+    public class User
     {
         public int Id { get; set; }
         public string Username { get; set; }
---
+++ MobEye/RestfulAPITests/Models/AlarmTests.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RestfulAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RestfulAPI.Models.Tests
+{
+    [TestClass()]
+    public class AlarmTests
+    {
+        [TestMethod()]
+        public void EmptyConstructorTest()
+        {
+            Alarm alarm = new Alarm();
+            Assert.IsNotNull(alarm);
+        }
+
+        [TestMethod()]
+        public void ConstructorTest()
+        {
+            Alarm alarm = new Alarm("alarm1", "Eindhoven", "Temperature is too high", "123");
+            Assert.AreEqual(alarm.deviceName, "alarm1");
+            Assert.AreEqual(alarm.location, "Eindhoven");
+            Assert.AreEqual(alarm.alarmText, "Temperature is too high");
+            Assert.AreEqual(alarm.accountId, "123");
+        }
+
+        [TestMethod()]
+        public void SetDeviceNameTest()
+        {
+            Alarm alarm = new Alarm();
+            alarm.deviceName = "alarm1";
+            Assert.AreEqual(alarm.deviceName, "alarm1");
+        }
+
+        [TestMethod()]
+        public void SetLocationTest()
+        {
+            Alarm alarm = new Alarm();
+            alarm.location = "Eindhoven";
+            Assert.AreEqual(alarm.location, "Eindhoven");
+        }
+
+        [TestMethod()]
+        public void SetAlarmTextTest()
+        {
+            Alarm alarm = new Alarm();
+            alarm.alarmText = "Temperature is too high";
+            Assert.AreEqual(alarm.alarmText, "Temperature is too high");
+        }
+
+        [TestMethod()]
+        public void SetAccountIdTest()
+        {
+            Alarm alarm = new Alarm();
+            alarm.accountId = "123";
+            Assert.AreEqual(alarm.accountId, "123");
+        }
+
+
+
+    }
+}
