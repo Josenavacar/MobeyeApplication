0c09e4d512400ea5feae8db62bd087eec8dc43ad
Morenc,Felix F <f.morenc@student.fontys.nl>
2020-11-25 16:54:17 +0100

    Dutch Translation and Language Selector Implemented

--- MobEye/MobEye/MobEye/Views/StartPage.xaml.cs
+++ MobEye/MobEye/MobEye/Views/StartPage.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using MobEye.Resources;
+using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -30,6 +32,30 @@ namespace MobEye.Views
         private async void DebugHome(object sender, EventArgs e)
         {
             await Navigation.PushAsync(new HomePage());
+        }
+
+        void ChangeLanguage(object sender, EventArgs args)
+        {
+            Picker languageSelect = (Picker)sender;
+
+            int language = languageSelect.SelectedIndex;
+
+            switch(language) 
+            {
+                case 0:
+                    AppResources.Culture = new CultureInfo("en");
+                    Navigation.PushAsync(new StartPage());
+                break;
+                case 1:
+                    AppResources.Culture = new CultureInfo("fr");
+                    Navigation.PushAsync(new StartPage());
+                break;
+                case 2:
+                    AppResources.Culture = new CultureInfo("nl");
+                    Navigation.PushAsync(new StartPage());
+                break;
+
+            }
         }
     }
 }
